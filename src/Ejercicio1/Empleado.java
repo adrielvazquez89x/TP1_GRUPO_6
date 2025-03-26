c9339b971859d54d40ebde46050d0ba84d348097
Sebastian Berti <sebaberti.8@gmail.com>
2025-03-26 19:40:22 -0300

    Creacion del primer constructor

--- src/Ejercicio1/Empleado.java
+++ src/Ejercicio1/Empleado.java
@@ -3,10 +3,28 @@ package Ejercicio1;
 public class Empleado {
 
 	//2. Crear una clase Empleado con las siguientes propiedades:
+	//3. Aplicar el concepto de encapsulación dentro de la clase Empleado
+	
 	private final int id;
 	private String nombre;
 	private int edad;
 	
-	private static int cont=1000;
+	private static int cont=999;
+	
+	//CONSTRUCTORES
+	public Empleado()
+	{
+		cont++;
+		this.id = cont;
+		this.nombre = "sin nombre";
+		this.edad = 99;
+	}
+
+	
+	
+	
+	
+	
+	
 	
 }
