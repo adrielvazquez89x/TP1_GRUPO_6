d969ad79de9a4276038243faabfacd7b405831ef
marinamjk <marinamjk@gmail.com>
2025-03-26 15:59:22 -0300

    creacion del contador

--- src/Ejercicio1/Empleado.java
+++ src/Ejercicio1/Empleado.java
@@ -3,7 +3,10 @@ package Ejercicio1;
 public class Empleado {
 
 	//2. Crear una clase Empleado con las siguientes propiedades:
-	private int id;
+	private final int id;
 	private String nombre;
 	private int edad;
+	
+	private static int cont=1000;
+	
 }
