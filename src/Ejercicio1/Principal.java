9e075b8c837b441f251e23775ae30709a99d0cf6
BILBODEVE <facundotaraujo@hotmail.com>
2025-03-27 20:03:16 -0300

    Creacion de objeto 4 y 5 + impresion de datos

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -8,22 +8,37 @@ public class Principal {
 	uno.setNombre("Jose");
 	uno.setEdad(30);
 	
-	Empleado dos = new Empleado("Agustina",31);
+	Empleado dos = new Empleado("Agustina",-1);
+	Empleado tres = new Empleado("Jesus", 33);
+	Empleado cuatro = new Empleado("Santiago", 11);
+	Empleado cinco = new Empleado();
+	cinco.setNombre("Soledad");
+	cinco.setEdad(25);
 	
-	//Opcion de usar array para guardar los 5 empleados (completar los faltantes si lo usamos)
+	System.out.println("IMPRESION DE FORMA INDIVIDUAL: \n");
+	System.out.println(uno.toString());	
+	System.out.println(dos.toString());
+	System.out.println(tres.toString());
+	System.out.println(cuatro.toString());
+	System.out.println(cinco.toString());
+	System.out.println("-------------------------\n");
+	
+	//Opcion de usar array para guardar los 5 empleados
 	Empleado[] Vempleados = new Empleado[5];
 	Vempleados[0]= uno;
 	Vempleados[1]= dos;	
 	Vempleados[2] = new Empleado("Jose",18);
+	Vempleados[3] = new Empleado("Marta", 105);
+	Vempleados[4] = new Empleado("Roberto", 50);
 
-
+	System.out.println("IMPRESION UTILIZANDO FOR EACH: \n");
 	//Otra opcion para imprimir la info:
 	for (Empleado e : Vempleados) {
 		System.out.println(e.toString());
 	}
+	System.out.println("-------------------------\n");
 	
-	
-	System.out.println("EL proximo id es: " + Empleado.devuelveProximoID());
+	System.out.println("El proximo id es: " + Empleado.devuelveProximoID());
 		
 	}
 
