f30e59a881c6d0cee974cf58ce14f16650feda51
Agus <142065648+agusdeansodev@users.noreply.github.com>
2025-03-27 13:36:13 -0300

    Update Principal.java
    Se agrego el empleado "dos" utilizando el constructor con parametros y el metodo toString para imprimirlo por consola

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -4,10 +4,14 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		Empleado uno= new Empleado();
-		System.out.println(uno.toString());
+	Empleado uno= new Empleado();
+	Empleado dos = new Empleado("Agustina",31);
 
-		System.out.println(Empleado.devuelveProximoID());
+	System.out.println(uno.toString());
+
+	System.out.println(Empleado.devuelveProximoID());
+
+	System.out.println(dos.toString());
 	}
 
 }
